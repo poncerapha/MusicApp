6287437c307ed55c9d60f2c00ee4716385904aed
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-10-05 08:48:44 -0300

    feat: create models

---
+++ MusicApp/src/br/com/alura/musicapp/models/Audio.java
@@ -0,0 +1,10 @@
+package br.com.alura.musicapp.models;
+
+public class Audio {
+
+    private String title;
+    private int duration;
+    private int totalPlays;
+    private int likes;
+    private int rating;
+}
---
+++ MusicApp/src/br/com/alura/musicapp/models/Podcast.java
@@ -0,0 +1,4 @@
+package br.com.alura.musicapp.models;
+
+public class Podcast extends Audio {
+}
